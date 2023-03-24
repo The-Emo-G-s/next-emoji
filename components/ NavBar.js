2f615d8d5d7ffed86953638a4bb4cbb6b2bdaf8e
Lai Saechao-Bazemore <saech@Lais-MacBook-Pro.local>
2023-03-24 16:07:40 -0400

    changed name

--- components/ NavBar.js
+++ components/ NavBar.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 const Navbar = () => {
 return (
   <div className="container-nav">
-    <Link href='/' className="link-home">The Em🤠ji Sh🛒p</Link>
+    <Link href='/' className="link-home">Clicker Kingd🦁m</Link>
     <Link href='/login' className="login-signup">🪵 Login / 🪧 Signup</Link>
 
   </div>
