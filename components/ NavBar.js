bc71c03f8f578ef318631d6e0aca9419f3243e10
Craig <71907929+jabaliCraig@users.noreply.github.com>
2023-03-30 14:50:16 -0600

    prepped banner for emoji carousel

--- components/ NavBar.js
+++ components/ NavBar.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 const Navbar = () => {
 return (
   <div className="container-nav">
-    <span><Link href='/' className="link-home">Clicker Kingdom</Link></span>
+    <span><Link href='/' className="link-home"><h1>CLICKER KINGD<span>O</span><M></M></h1></Link></span>
   </div>
 )
 }
