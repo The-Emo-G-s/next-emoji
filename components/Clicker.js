50a0e1479fdd5af82e8d63e4776449fb1c8e2a11
Lai Saechao-Bazemore <saech@Lais-MacBook-Pro.local>
2023-03-30 20:37:07 -0400

    increment working sorta and added save button

--- components/Clicker.js
+++ components/Clicker.js
@@ -4,16 +4,6 @@ import { useRouter } from 'next/router';
 import {  useUser, useSupabaseClient  } from '@supabase/auth-helpers-react'
 
 
-const upgrades = [
-  { name: "🐌 Boost", description: "+1 point per click", cost: 100 },
-  {
-    name: "🐛 Boost",
-    description: "Automatically click every 2 seconds",
-    cost: 1000,
-  },
-  { name: "🦋 Boost", description: "+1 point per second", cost: 10000 },
-];
-
 export default function Clicker ({session}) {
   const supabase = useSupabaseClient()
   const user = useUser()
@@ -21,10 +11,7 @@ export default function Clicker ({session}) {
   const [username, setUsername] = useState(null)
   const [avatar_url, setAvatarUrl] = useState(null)
   const [loading, setLoading] = useState(true)
-  const [clickerCount, setClickerCount] = useState(0);
-  const [autoCount, setAutoCount] = useState(0);
-  const [idleCount, setIdleCount] = useState(0);
-  const [purchasedUpgrades, setPurchasedUpgrades] = useState({});
+
 
   useEffect(() => {
     getGame()
@@ -96,72 +83,60 @@ async function getCurrentUser() {
       setLoading(false)
     }
   }
-
-	async function resetGame() {
-		try {
-			setLoading(true)
-			setPoints(0)
-			console.log(points)
-			const updates = {
-				id: user.id,
-				points,
-				updated_at: new Date().toISOString(),
-			}
-
- await supabase.from('profiles').upsert(updates)
-			console.log('Point added!')
-		} catch (error) {
-			alert('Error updating the data!')
-			console.log(error)
-		} finally {
-			setLoading(false)
-		}
-	}
-  // const handleReset = async () => {
-  //   const { data, error } = await supabase
-  //     .from('users')
-  //     .update({ points: 0 })
-  //     .eq('id', user.id)
-  //     .single();
-
-  //   if (error) {
-  //     console.error(error);
-  //     return;
-  //   }
-
-  //   setPoints(data.points);
-  // };
-  const purchaseUpgrade = (upgrade) => {
-    if (points >= upgrade.cost) {
-      setPoints(points - upgrade.cost);
-
-      switch (upgrade.name) {
-        case "Click Boost":
-          setClickerCount(clickerCount + 1);
-          break;
-        case "Auto Clicker":
-          setAutoCount(autoCount + 1);
-          break;
-        case "Idle Boost":
-          setIdleCount(idleCount + 1);
-          break;
-        default:
-          break;
+  async function auto({points}) {
+    try {
+      setLoading(true)
+      if(points >= 10){
+const interval = setInterval(() => {
+  setPoints((points) => points+ 1)
+}, 1000)}
+      const updates = {
+        id: user.id,
+        points: points,
+        updated_at: new Date().toISOString(),
       }
 
-      setPurchasedUpgrades({
-        ...purchasedUpgrades,
-        [upgrade.name]: purchasedUpgrades[upgrade.name] + 1 || 1,
-      });
+ await supabase.from('profiles').upsert(updates)
+      // alert('Point added!')
+    } catch (error) {
+      alert('Error updating the data!')
+      console.log(error)
+    } finally {
+      setLoading(false)
     }
-  };
+
+  }
+
+  async function save({points}) {
+    try {
+      console.log(points, 'first')
+  setPoints(points)
+  console.log(points, 'points')
+      const updates = {
+        id: user.id,
+        points: points,
+        updated_at: new Date().toISOString(),
+      }
+
+ await supabase.from('profiles').upsert(updates)
+      // alert('Point added!')
+    } catch (error) {
+      alert('Error updating the data!')
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+
+  }
 
   return (
     <div>
-			{avatar_url?.slice(0, 35)==='https://em-content.zobj.net/thumbs/' 
+			{avatar_url?.slice(0, 35)==='https://em-content.zobj.net/thumbs/'
 				? <>
 						<h1>Click Away{username && `, ${username}`}!</h1>
      		 		<p>Points: {points.toLocaleString("en-US")}</p>
+              <button onClick={() => save({points})}> 🛟 Save</button>
+              <br></br>
 						<button
 								id='emoji-button'
 								onClick={() => updateGame({points})}
@@ -169,26 +144,15 @@ async function getCurrentUser() {
 									<img src={avatar_url}/>
 						</button>
 						<br></br>
-						<h2> 🌬 Boost:</h2>
-						{upgrades.map((upgrade) => (
-							<button key={upgrade.name}
-								onClick={() => purchaseUpgrade(upgrade)}
-								disabled={points < upgrade.cost}>
-								{upgrade.name} ({upgrade.cost} points):{" "}
-								{purchasedUpgrades[upgrade.name] || 0} purchased (
-								{upgrade.description})
-							</button>
-						))}
-					</> 
+					</>
 				: <div>
 						<h1>Head to the ACCOUNT link above to set your ANIMOJI, then come back to start clicking!!!!</h1>
 						{/* insert carousel for bored eyes */}
 					</div>
 			}
 
-		<button onClick={() => resetGame(points)}>Reset Points</button>
-			
 
+<button onClick={() => auto({points})}> 🚀 Activate Boost</button>
     </div>
   );
 }
